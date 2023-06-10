87f3867400db113ff6a51cd9fc9a6f7f6c70b5e6
Ivica Koteski <koteski.ivica@uklo.edu.mk>
2023-06-10 12:18:38 +0200

    Password Reset Activity

--- app/src/main/java/com/example/mychatapp/SignUpActivity.java
+++ app/src/main/java/com/example/mychatapp/SignUpActivity.java
@@ -3,12 +3,30 @@ package com.example.mychatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private CircleImageView imageViewCircle;
+    private TextInputEditText editTextEmailSignUp,editTextPasswordSignup,editTextUserNameSignup;
+    private Button buttonRegister;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        imageViewCircle = findViewById(R.id.imageViewCircle);
+        editTextEmailSignUp = findViewById(R.id.editTextEmailSignup);
+        editTextPasswordSignup = findViewById(R.id.editTextPasswordSignup);
+        editTextUserNameSignup = findViewById(R.id.editTextUserNameSignup);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
     }
 }
