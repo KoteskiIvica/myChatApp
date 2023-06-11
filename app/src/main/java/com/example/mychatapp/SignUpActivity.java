478e12183c83fabb911611196a3ac8a1599e7fb8
Ivica Koteski <koteski.ivica@uklo.edu.mk>
2023-06-11 17:46:21 +0200

    Write user in DB

--- app/src/main/java/com/example/mychatapp/SignUpActivity.java
+++ app/src/main/java/com/example/mychatapp/SignUpActivity.java
@@ -16,9 +16,23 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -29,6 +43,14 @@ public class SignUpActivity extends AppCompatActivity {
     private Button buttonRegister;
     boolean imageControl = false;
     ActivityResultLauncher<Intent> activityResultLauncher;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    FirebaseStorage fireBaseStorage;
+    StorageReference storageReference;
+    Uri imageUri;
 
 
 
@@ -45,6 +67,14 @@ public class SignUpActivity extends AppCompatActivity {
         editTextPasswordSignup = findViewById(R.id.editTextPasswordSignup);
         editTextUserNameSignup = findViewById(R.id.editTextUserNameSignup);
         buttonRegister = findViewById(R.id.buttonRegister);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        fireBaseStorage = FirebaseStorage.getInstance();
+        storageReference = fireBaseStorage.getReference();
+
 
 
 
@@ -72,7 +102,7 @@ public class SignUpActivity extends AppCompatActivity {
     }
     public void imageChooser() {
        Intent intent = new Intent();
-       intent.setType("images/*");
+       intent.setType("image/*");
        intent.setAction(Intent.ACTION_GET_CONTENT);
        //startActivityForResult(intent,1);
         activityResultLauncher.launch(intent);
@@ -87,7 +117,7 @@ public class SignUpActivity extends AppCompatActivity {
                         Intent data = result.getData();
 
                         if (resultCode == RESULT_OK && data != null) {
-                            Uri imageUri = data.getData();
+                            imageUri = data.getData();
                             Picasso.get().load(imageUri).into(imageViewCircle);
                             imageControl = true;
                         } else {
@@ -112,7 +142,57 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }*/
 
-    public void  signup(String email, String password, String username){
+    public void  signup(String email, String password, String userName){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    reference.child("Users").child(auth.getUid()).child("userName").setValue(userName);
+
+                    if (imageControl){
+                        UUID randomID = UUID.randomUUID();
+                        String imageName = "images/"+randomID+".jpg";
+                        storageReference.child(imageName).putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                StorageReference myStorageRef = fireBaseStorage.getReference(imageName);
+                                myStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String filePath = uri.toString();
+                                        reference.child("Users").child(auth.getUid()).child("image").setValue(filePath).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(SignUpActivity.this,"Write to database is success",Toast.LENGTH_SHORT).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(Exception e) {
+                                                Toast.makeText(SignUpActivity.this,"Write to database is not successful",Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+
+                    }
+                    else {
+                            reference.child("Users").child(auth.getUid()).child("image").setValue("null");
+                    }
+                    Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                    intent.putExtra("userName",userName);
+                    startActivity(intent);
+                    finish();
+
+
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this,"There is a problem",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
