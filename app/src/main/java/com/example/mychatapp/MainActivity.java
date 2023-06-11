67d210cc994357c77e042013f50274251480fa4e
Ivica Koteski <koteski.ivica@uklo.edu.mk>
2023-06-11 19:28:12 +0200

    Add ProfileActivity

--- app/src/main/java/com/example/mychatapp/MainActivity.java
+++ app/src/main/java/com/example/mychatapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.mychatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.chat_menu,menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_profile){
+            startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+        }
+        if(item.getItemId() == R.id.action_signout){
+            auth.signOut();
+            startActivity(new Intent(MainActivity.this,MyLoginActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
