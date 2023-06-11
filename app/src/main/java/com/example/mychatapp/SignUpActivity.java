82cff69b45e1a491b6acd52a87a2a9d6d5922015
Ivica Koteski <koteski.ivica@uklo.edu.mk>
2023-06-11 11:26:50 +0200

    Sign up setup

--- app/src/main/java/com/example/mychatapp/SignUpActivity.java
+++ app/src/main/java/com/example/mychatapp/SignUpActivity.java
@@ -1,11 +1,24 @@
 package com.example.mychatapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -14,6 +27,8 @@ public class SignUpActivity extends AppCompatActivity {
     private CircleImageView imageViewCircle;
     private TextInputEditText editTextEmailSignUp,editTextPasswordSignup,editTextUserNameSignup;
     private Button buttonRegister;
+    boolean imageControl = false;
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
 
 
@@ -22,11 +37,82 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //register
+        registerActivityForSelectImage();
+
         imageViewCircle = findViewById(R.id.imageViewCircle);
         editTextEmailSignUp = findViewById(R.id.editTextEmailSignup);
         editTextPasswordSignup = findViewById(R.id.editTextPasswordSignup);
         editTextUserNameSignup = findViewById(R.id.editTextUserNameSignup);
         buttonRegister = findViewById(R.id.buttonRegister);
+
+
+
+        imageViewCircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageChooser();
+
+            }
+        });
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmailSignUp.getText().toString();
+                String password = editTextPasswordSignup.getText().toString();
+                String userName = editTextUserNameSignup.getText().toString();
+
+                if (!email.equals("")&& password.equals("")&& !userName.equals("")){
+                    signup(email,password,userName);
+                }
+
+            }
+        });
+
+    }
+    public void imageChooser() {
+       Intent intent = new Intent();
+       intent.setType("images/*");
+       intent.setAction(Intent.ACTION_GET_CONTENT);
+       //startActivityForResult(intent,1);
+        activityResultLauncher.launch(intent);
+
+   }
+
+   private void registerActivityForSelectImage(){
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        int resultCode = result.getResultCode();
+                        Intent data = result.getData();
+
+                        if (resultCode == RESULT_OK && data != null) {
+                            Uri imageUri = data.getData();
+                            Picasso.get().load(imageUri).into(imageViewCircle);
+                            imageControl = true;
+                        } else {
+                            imageControl = false;
+                        }
+                    }
+
+                });
+    }
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+            Uri imageUri = data.getData();
+            Picasso.get().load(imageUri).into(imageViewCircle);
+            imageControl = true;
+
+        }
+        else
+        {
+            imageControl = false;
+        }
+    }*/
+
+    public void  signup(String email, String password, String username){
 
     }
 }
